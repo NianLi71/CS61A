5cc68c936028d9083768544690952070b266a283
NianLi71 <linian0701@163.com>
2022-02-26 00:35:01 +0800

    add hw04 and lab10

---
+++ lab/lab10/lab10.scm
@@ -0,0 +1,11 @@
+(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+
+(define (make-adder num) 'YOUR-CODE-HERE)
+
+(define (composed f g) 'YOUR-CODE-HERE)
+
+(define lst 'YOUR-CODE-HERE)
+
+(define (remove item lst) 'YOUR-CODE-HERE)
+
+(+ 33 3)
